c78f12c35065a8a7ae037f7d39ed4b60acd54723
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-07-29 22:33:26 -0500

    List

--- 0x01-insert_in_sorted_linked_list/list.h
+++ 0x01-insert_in_sorted_linked_list/list.h
@@ -2,7 +2,7 @@
 #define LISTS_H
 
 #include <stddef.h>
-#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,8 +13,8 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +23,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif
+#endif /* LISTS_H */
