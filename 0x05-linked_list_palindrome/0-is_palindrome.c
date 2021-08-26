398c33bae7f5b713ffbd4254cf8bbd96698d6adb
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-08-26 08:55:33 -0500

    0Is

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,57 +2,28 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to head of list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
 
+/**
+ * is_palindrome - Function that checks if a singly linked list is palindrome.
+ * @head: Pointer to Linked list.
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
+ */
 int is_palindrome(listint_t **head)
 {
-listint_t *left = *head;
-listint_t *right = *head;
-int len = 0, num;
+	listint_t *tmp = *head;
+	int i = 0, j = 0, k[1000000], len = 0;
 
-left = *head;
-num = recursive(left, right, len);
-if (num || *head == NULL)
-return (1);
-return (0);
-}
-
-/**
- * recursive - recursive function
- * @left: left head node
- * @right: right head node
- * @len: len list
- *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
-
-int recursive(listint_t *left, listint_t *right, int len)
-{
-int count = 0, num = len;
-if (right != NULL)
-{
-len++;
-num = recursive(left, right->next, len);
-if (num / 2 < len)
-return (num);
-if (num == 0)
-return (0);
-}
-else
-return (num);
-
-while (count < num - len)
-{
-left = left->next;
-count++;
-}
-
-if (left->n == right->n)
-return (num);
-return (0);
+	if (head == NULL)
+		return (0);
+	for (len = 0; tmp != NULL; len++)
+	{
+		k[len] = tmp->n;
+		tmp = tmp->next;
+	}
+	for (i = 0, j = len - 1; i < (len / 2); i++, j--)
+	{
+		if (k[i] != k[j])
+			return (0);
+	}
+	return (1);
 }
