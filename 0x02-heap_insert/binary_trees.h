239bdfbb9bc2986b0545b77ce7b64a125128dad8
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-08-31 17:47:41 -0500

    Header update

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,7 +1,6 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -12,21 +11,16 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-
 typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+} heap_t, binary_tree_t;
 
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
-heap_t *heap_insert(heap_t **root, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *, int);
+heap_t *heap_insert(heap_t **, int);
 
-#endif /*_BINARY_TREES_H_*/
+#endif
