3582873b46eb8f6e6d8ca3bd3fbe3bacf7712556
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-10-21 22:25:40 -0500

    Upt

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,90 +1,80 @@
 #!/usr/bin/python3
-"""
-N Queens
-"""
+""" The N queens puzzle is the challenge of placing N
+     non-attacking queens on an N×N chessboard.
+     Write a program that solves the N queens problem."""
+
 import sys
 
 
 if len(sys.argv) != 2:
-    print('Usage: nqueens N')
+    print("Usage: nqueens N")
     exit(1)
-N = sys.argv[1]
-try:
-    N = int(N)
-except ValueError:
-    print('N must be a number')
+if not sys.argv[1].isdigit():
+    print("N must be a number")
     exit(1)
-
-if N < 4:
-    print('N must be at least 4')
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
     exit(1)
-
+N = int(sys.argv[1])
+board = [[0 for i in range(N)]for j in range(N)]
 k = 1
 
 
-def printSolution(board):
-    """
-    print Solution
-    """
-    queens = []
+def print_sol(board):
+    """ Method Print"""
     global k
     k = k + 1
+    S = []
     for i in range(N):
         for j in range(N):
             if board[i][j] == 1:
-                queens.append([i, j])
-    print(queens)
+                S.append([i, j])
+    print(S)
 
 
 def isSafe(board, row, col):
-    """
-    isSafe
-    """
+    """ Method IsSafe """
     for i in range(col):
-        if board[row][i]:
+        if (board[row][i]):
             return False
     i = row
     j = col
     while i >= 0 and j >= 0:
-        if board[i][j]:
+        if(board[i][j]):
             return False
         i -= 1
         j -= 1
     i = row
     j = col
     while j >= 0 and i < N:
-        if board[i][j]:
+        if(board[i][j]):
             return False
         i = i + 1
         j = j - 1
     return True
 
 
-def solveNQUtil(board, col):
-    """
-    solve NQtil
-    """
-    if col == N:
-        printSolution(board)
+def solving(board, col):
+    """ Method Solving """
+    if (col == N):
+        print_sol(board)
         return True
     res = False
     for i in range(N):
-        if isSafe(board, i, col):
+        if (isSafe(board, i, col)):
             board[i][col] = 1
-            res = solveNQUtil(board, col + 1) or res
+            res = solving(board, col + 1) or res
             board[i][col] = 0
     return res
 
 
-def solveNQ():
-    """
-    solve NQ
-    """
-    board = [[0 for j in range(N)] for i in range(N)]
-    if solveNQUtil(board, 0) is False:
-        pass
+def solve():
+    """ Method Solve"""
+    board = [[0 for j in range(N)]for i in range(N)]
+    if (solving(board, 0) is False):
+        print("Solution does not exist")
         return
     return
 
 
-solveNQ()
+solve()
